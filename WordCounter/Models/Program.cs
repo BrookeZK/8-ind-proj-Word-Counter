b3c92aa0ee375e941ce0d474a18012a0afa04bb1
Student <example@example.com>
2020-03-06 16:04:46 -0800

    Added program file and methods

--- WordCounter/Models/Program.cs
+++ WordCounter/Models/Program.cs
@@ -8,19 +8,34 @@ namespace WordCounter
 	{
 		 public static void Main()
 		{
-				Console.WriteLine("Enter a sentence");
-				string userSentence = Console.ReadLine();
-				Console.WriteLine("Enter a word you would like to count in your sentence");
-				string userWord = Console.ReadLine();
+				Console.ForegroundColor = ConsoleColor.DarkYellow;
+				Console.WriteLine(@" ____ ____ ____ ____ _________ ____ ____ ____ ____ ____ ____ ____ 
+||W |||O |||R |||D |||       |||C |||H |||E |||C |||K |||E |||R ||
+||__|||__|||__|||__|||_______|||__|||__|||__|||__|||__|||__|||__||
+|/__\|/__\|/__\|/__\|/_______\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|");
 
-				Sentence newSentence = new Sentence(userWord, userSentence);
-				string cleanUserSentence = newSentence.SentenceCleaner(userSentence);
-				string cleanUserWord = newSentence.WordCleaner(userWord);
+		}
+		public static void InputGather()
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("Where we count the frequency of words in a sentence!");
+			Console.WriteLine("Enter a sentence:");
+			string userSentence = Console.ReadLine();
+			Console.ForegroundColor = ConsoleColor.Blue;
+			Console.WriteLine("Enter a word you would like to count in your sentence:");
+			string userWord = Console.ReadLine();
+			DisplayOutput(userWord, userSentence);
+		}
+		public static void DisplayOutput(string userWord, string userSentence)
+		{
+			Sentence newSentence = new Sentence(userWord, userSentence);
+			string cleanUserSentence = newSentence.SentenceCleaner(userSentence);
+			string cleanUserWord = newSentence.WordCleaner(userWord);
 
-				string[] userSentenceArray = newSentence.SentenceToWordArray(cleanUserSentence);
+			string[] userSentenceArray = newSentence.SentenceToWordArray(cleanUserSentence);
 
-
-				Console.WriteLine(newSentence.SentenceChecker(userSentenceArray, cleanUserWord));
+			Console.WriteLine("================================================");
+			Console.WriteLine("Total count of: " + (userWord) + " in the sentence " + (userSentence) + " : " + newSentence.SentenceChecker(userSentenceArray, cleanUserWord));
 		}
 	}
 }
